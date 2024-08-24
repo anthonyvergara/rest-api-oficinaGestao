64bb208f00ec0394f811a3452fbe46ff187bea56
vixithony <vixithony@gmail.com>
2024-08-23 22:31:14 -0300

    First commit

---
+++ src/main/java/com/api/oficina/ApiOficinaApplication.java
@@ -0,0 +1,26 @@
+package com.api.oficina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.api.oficina.model"})
+@ComponentScan(basePackages = {"com.*"}) //Realizar injeção de dependências.
+@EnableJpaRepositories(basePackages = {"com.api.oficina.repository"}) // Habilita a funcionalidade das interfaces de persistência.
+@EnableTransactionManagement// Gerenciador de transações
+// @EnableWebMvc // Ativa o projeto de MVC
+@RestController
+@EnableAutoConfiguration // Configura todo o projeto (manipulações de classes, dependencias...)
+public class ApiOficinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOficinaApplication.class, args);
+	}
+
+}
