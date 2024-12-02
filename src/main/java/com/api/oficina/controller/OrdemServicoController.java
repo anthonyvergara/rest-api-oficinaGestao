460bc54a06850f69cb835777ab350d68a3ed2a13
vixithony <vixithony@gmail.com>
2024-12-02 20:34:06 -0300

    Fix: included cross origin permission

--- src/main/java/com/api/oficina/controller/OrdemServicoController.java
+++ src/main/java/com/api/oficina/controller/OrdemServicoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import jakarta.validation.Valid;
 @RestController
 @Validated
 @RequestMapping(value = "/oficina/ordemServico")
-//@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrdemServicoController {
 
 	private final OrdemServicoImpl ORDEM_SERVICO;
