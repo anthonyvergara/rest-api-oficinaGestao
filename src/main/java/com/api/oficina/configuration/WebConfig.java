f7ae27c81da41d9e3caed5272a2094e0d64dcca5
vixithony <vixithony@gmail.com>
2025-03-23 18:14:29 -0300

    Chore: enviroments

--- src/main/java/com/api/oficina/configuration/WebConfig.java
+++ src/main/java/com/api/oficina/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package com.api.oficina.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +10,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	
+	private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+	
 	@Value("${cliente.origin}")
     private String clienteOrigin;
-
+	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
+		logger.info("Cliente Origin: {}", clienteOrigin);  
+		
         registry.addMapping("/**")
                 .allowedOrigins(clienteOrigin)  // Usando a variável de ambiente
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+	
+	
+	
 
 }
